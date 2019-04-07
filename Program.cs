516df58f8b1b7e442e9a89fd662542d2d63c95f8
Daniel Sobczak <gubuv@red-mail.info>
2019-04-07 16:37:18 +0200

    instumentation added

--- Program.cs
+++ Program.cs
@@ -6,27 +6,54 @@ namespace search
     {
         public static void binarySearch(int l, int r, int[] array, int m, int toSearch)
         {
+            int operationCount = 0;
             do
             {
                 m = (l+r) / 2;
                 if (array[m] == toSearch)
                 {
+                    operationCount++;
                     System.Console.WriteLine("Element {0} znaleziony w indeksie {1}", toSearch, m);
                     break;
                 }
                 else if (array[m] != toSearch)
-                    if (array[m] > toSearch)
+                    if (array[m] > toSearch) {
                         r = m - 1;
-                    else
+                        operationCount++;
+                    }
+                    else {
                         l = m + 1;
+                        operationCount++;
+                    }
             } while (!(l > r));
             if (l > r)
                 System.Console.WriteLine("Nie znaleziono podanego elementu");
+
+            System.Console.WriteLine("Liczba operacji: {0}", operationCount);
         }
         
         public static void Main(string[] args)
         {
-            int[] table = {1, 2, 5, 8, 9, 11, 15, 20};
+            int count = 1;            
+            for (int k = 2000000; k < (int) Math.Pow(2, 28); k += 100000)
+            {
+                int[] tab = new int[k];
+                for (int i = 0; i < tab.Length; i++)
+                {
+                    tab[i] = i+1;
+                }
+                System.Console.WriteLine("Utworzona tablicę nr {0}", count);
+                count++;
+
+                int left = 0;
+                int right = tab.Length - 1;
+                int middle = (left + right) / 2;
+
+                binarySearch(left, right, tab, middle, 1) ;
+            }
+            Console.ReadKey();
+
+            /* int[] table = {1, 2, 5, 8, 9, 11, 15, 20};
 
             int left = 0;
             int right = table.Length - 1;
@@ -35,13 +62,9 @@ namespace search
             System.Console.WriteLine("Podaj liczbę do wyszukania");
             int number = int.Parse(Console.ReadLine());
 
-            /* if (table[middle] == number)
-                System.Console.WriteLine("Liczba jest w środku tablicy");
-            else */
             binarySearch(left, right, table, middle, number) ;           
 
-            //System.Console.WriteLine(middle);
-            Console.ReadKey();
+            Console.ReadKey(); */
         }
     }
 }
