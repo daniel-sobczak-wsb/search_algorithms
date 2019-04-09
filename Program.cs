6c9aceaa33807d427649ec5387f395855f23cc86
Daniel Sobczak <gubuv@red-mail.info>
2019-04-09 16:07:25 +0200

    binary search avg case

--- Program.cs
+++ Program.cs
@@ -5,16 +5,19 @@ namespace search
 {
     class Program
     {
-        public static void binarySearch(int l, int r, int[] array, int m, int toSearch)
+        public static (int result, int opernum) binarySearch(int l, int r, int[] array, int m, int toSearch)
         {
             int operationCount = 0;
+            int resultCode = 0;
+
             do
             {
                 m = (l+r) / 2;
                 if (array[m] == toSearch)
                 {
                     operationCount++;
-                    System.Console.WriteLine("Element {0} znaleziony w indeksie {1}", toSearch, m);
+                    //System.Console.WriteLine("Element {0} znaleziony w indeksie {1}", toSearch, m);
+                    resultCode = 1;
                     break;
                 }
                 else if (array[m] != toSearch)
@@ -28,52 +31,47 @@ namespace search
                     }
             } while (!(l > r));
             if (l > r)
+            {
                 System.Console.WriteLine("Nie znaleziono podanego elementu");
-
-            System.Console.WriteLine("Liczba operacji: {0}", operationCount);
+                resultCode = -1;
+            }
+            //System.Console.WriteLine("Liczba operacji: {0}", operationCount);
+           
+           //return operationCount;
+           return (result: resultCode, opernum: operationCount);
         }
-        
+       
         public static void Main(string[] args)
         {
+            Random rand = new Random();
             int count = 1;  
             double elapsedSeconds = 0;
             double elapsedTime = 0;          
             for (int k = 2000000; k < (int) Math.Pow(2, 28); k += 100000)
             {
                 int[] tab = new int[k];
+                
                 for (int i = 0; i < tab.Length; i++)
                 {
                     tab[i] = i+1;
                 }
-                System.Console.WriteLine("Utworzona tablicę nr {0}", count);
+                //System.Console.WriteLine("Utworzona tablicę nr {0}", count);
                 count++;
 
+                int number = rand.Next(1, tab.Length);
                 int left = 0;
                 int right = tab.Length - 1;
                 int middle = (left + right) / 2;
-
+ 
                 long start = Stopwatch.GetTimestamp();
-                binarySearch(left, right, tab, middle, 1) ;
+                (int result, int opernum) = binarySearch(left, right, tab, middle, number) ;
                 long stop = Stopwatch.GetTimestamp();
-
+ 
                 elapsedTime = stop - start;
                 elapsedSeconds = elapsedTime * (1.0 / Stopwatch.Frequency);
-                System.Console.WriteLine(elapsedSeconds);
+                System.Console.WriteLine("{0};{1};{2:F8};{3};{4}", k, number, elapsedSeconds, result, opernum);
             }
             Console.ReadKey();
-
-            /* int[] table = {1, 2, 5, 8, 9, 11, 15, 20};
-
-            int left = 0;
-            int right = table.Length - 1;
-            int middle = (left + right) / 2;
-
-            System.Console.WriteLine("Podaj liczbę do wyszukania");
-            int number = int.Parse(Console.ReadLine());
-
-            binarySearch(left, right, table, middle, number) ;           
-
-            Console.ReadKey(); */
         }
     }
 }
